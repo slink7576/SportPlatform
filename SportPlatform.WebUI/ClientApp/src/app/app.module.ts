bdf85bce09d4793c454a09e9389ea3a0393342d8
Sergiy Popovych <slnkonline2@gmail.com>
2018-11-24 21:49:44 +0200

    Generated swagger client and implemented it for users

--- SportPlatform.WebUI/ClientApp/src/app/app.module.ts
+++ SportPlatform.WebUI/ClientApp/src/app/app.module.ts
@@ -9,7 +9,7 @@ import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-
+import { Client } from './sport-platform-api';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,7 +28,7 @@ import { FetchDataComponent } from './fetch-data/fetch-data.component';
       { path: 'fetch-data', component: FetchDataComponent },
     ])
   ],
-  providers: [],
+  providers: [Client],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- SportPlatform.WebUI/ClientApp/src/app/fetch-data/fetch-data.component.ts
+++ SportPlatform.WebUI/ClientApp/src/app/fetch-data/fetch-data.component.ts
@@ -1,23 +1,18 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Client, UsersListViewModel, UserDTO } from '../sport-platform-api';
 
 @Component({
   selector: 'app-fetch-data',
   templateUrl: './fetch-data.component.html'
 })
 export class FetchDataComponent {
-  public forecasts: WeatherForecast[];
+  public users: UserDTO[];
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<WeatherForecast[]>(baseUrl + 'api/SampleData/WeatherForecasts').subscribe(result => {
-      this.forecasts = result;
-    }, error => console.error(error));
+  constructor(client: Client) {
+    client.get().subscribe(result => {
+      this.users = result.users;
+    }, error => console.log(error));
   }
 }
 
-interface WeatherForecast {
-  dateFormatted: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
