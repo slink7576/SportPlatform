c10175e7e1ed87abf12d6bbf0cdbebd22810f88d
Sergiy Popovych <slnkonline2@gmail.com>
2018-11-24 21:51:02 +0200

    Added swagger middleware

--- SportPlatform.WebUI/Startup.cs
+++ SportPlatform.WebUI/Startup.cs
@@ -1,10 +1,17 @@
+using MediatR;
+using MediatR.Pipeline;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using SportPlatform.Application.Users.Queries.GetUser;
+using SportPlatform.Persistence;
+using Swashbuckle.AspNetCore.Swagger;
+using System.Reflection;
 
 namespace SportPlatform.WebUI
 {
@@ -20,7 +27,25 @@ namespace SportPlatform.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(RequestPreProcessorBehavior<,>));
+            services.AddMediatR(typeof(GetUserQueryHandler).GetTypeInfo().Assembly);
+
+            services.AddDbContext<SportPlatformContext>(options =>
+              options.UseSqlServer(Configuration.GetConnectionString("SportPlatformDatabase")));
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info
+                {
+                    Version = "v1",
+                    Title = "My API",
+                    Description = "Sport Platform API",
+                    TermsOfService = "None",
+                    Contact = new Contact() { Name = "Sergiy Popovych", Email = "slnkonline2@gmail.com"}
+                });
+            });
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+             //   .AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<CreateCustomerCommandValidator>());;
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
@@ -52,7 +77,7 @@ namespace SportPlatform.WebUI
                     name: "default",
                     template: "{controller}/{action=Index}/{id?}");
             });
-
+            app.UseSwagger(c => { c.RouteTemplate = "api/swagger/{documentName}/swagger.json"; }); app.UseSwaggerUI(c => { c.SwaggerEndpoint("v1/swagger.json", "Some API"); c.RoutePrefix = "api/swagger"; });
             app.UseSpa(spa =>
             {
                 // To learn more about options for serving an Angular SPA from ASP.NET Core,
