966254dc017d7f4b20217a0a98d6351613f3b56f
Sergiy Popovych <slnkonline2@gmail.com>
2018-11-24 18:38:42 +0200

    Configured ef and User configuration for it

---
+++ SportPlatform.Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SportPlatform.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportPlatform.Persistence.Configurations
+{
+    public class UserConfiguration
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            // TODO: User configuration
+        }
+    }
+}
---
+++ SportPlatform.Persistence/SportPlatformContext.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportPlatform.Domain.Entities;
+using SportPlatform.Persistence.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportPlatform.Persistence
+{
+    public class SportPlatformContext : DbContext
+    {
+        public SportPlatformContext(DbContextOptions<SportPlatformContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Certificate> Certificates { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<DishIngridient> DishIngridients { get; set; }
+        public DbSet<DishMenu> DishMenus { get; set; }
+        public DbSet<FoodProgram> FoodPrograms { get; set; }
+        public DbSet<Ingridient> Ingridients { get; set; }
+        public DbSet<Menu> Menus { get; set; }
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<Training> Trainings { get; set; }
+        public DbSet<TrainingAction> TrainingActions { get; set; }
+        public DbSet<TrainingProgram> TrainingPrograms { get; set; }
+        public DbSet<Worker> Workers { get; set; }
+        public DbSet<WorkHistory> WorkHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyAllConfigurations();
+        }
+
+    }
+}
