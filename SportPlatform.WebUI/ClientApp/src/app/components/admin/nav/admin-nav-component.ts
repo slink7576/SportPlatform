b0cc77d924c11903c475fe4b6f9e98f1f15224ef
Sergiy Popovych <slnkonline2@gmail.com>
2018-11-25 16:32:45 +0200

    Added admin navigation component

---
+++ SportPlatform.WebUI/ClientApp/src/app/components/admin/nav/admin-nav-component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'admin-nav-component',
+  templateUrl: 'admin-nav-component.html',
+  styleUrls: ['../admin.css'],
+})
+
+export class AdminNavComponent {
+}
