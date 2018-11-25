97bb37fa2fa21a355c7b7c1b5111f413bab3ee89
Sergiy Popovych <slnkonline2@gmail.com>
2018-11-25 16:35:01 +0200

    Updated Angular, added angular material, removed standtart components

---
+++ SportPlatform.WebUI/ClientApp/src/validators/password-validator.ts
@@ -0,0 +1,14 @@
+import { AbstractControl } from '@angular/forms';
+
+export class PasswordValidation {
+
+    static MatchPassword(AC: AbstractControl) {
+       let Password = AC.get('Password').value; // to get value in input tag
+       let ConfirmPassword = AC.get('ConfirmPassword').value; // to get value in input tag
+        if(Password != ConfirmPassword) {
+            AC.get('ConfirmPassword').setErrors( {MatchPassword: true} )
+        } else {
+            return null
+        }
+    }
+}
