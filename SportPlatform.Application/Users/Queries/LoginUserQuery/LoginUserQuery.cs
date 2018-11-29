d75481077af6fa7212d558dcc57df420bb1f9b18
Sergiy Popovych <slnkonline2@gmail.com>
2018-11-29 21:53:02 +0200

    Added login user query

---
+++ SportPlatform.Application/Users/Queries/LoginUserQuery/LoginUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using SportPlatform.Application.Users.Queries.GetUser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportPlatform.Application.Users.Queries.LoginUserQuery
+{
+    public class LoginUserQuery : IRequest<bool>
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ SportPlatform.Application/Users/Queries/LoginUserQuery/LoginUserQueryHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SportPlatform.Application.Exceptions;
+using SportPlatform.Application.Users.Queries.GetUser;
+using SportPlatform.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SportPlatform.Application.Users.Queries.LoginUserQuery
+{
+    public class LoginUserQueryHandler : IRequestHandler<LoginUserQuery, bool>
+    {
+        private readonly SportPlatformContext _context;
+
+        public LoginUserQueryHandler(SportPlatformContext sportPlatformContext)
+        {
+            _context = sportPlatformContext;
+        }
+
+        public async Task<bool> Handle(LoginUserQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                 .Where(c => c.Email == request.Email &&
+                            c.Password == request.Password)
+                 .Select(UserViewModel.Projection)
+                 .SingleOrDefaultAsync(cancellationToken);
+
+            if (user == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
