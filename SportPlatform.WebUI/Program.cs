941ce4f5dc038e0915f3b4f1b6f795a656a2f78b
Sergiy Popovych <slnkonline2@gmail.com>
2018-11-29 21:46:54 +0200

    Added iis integration

--- SportPlatform.WebUI/Program.cs
+++ SportPlatform.WebUI/Program.cs
@@ -19,6 +19,8 @@ namespace SportPlatform.WebUI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
+            .UseIISIntegration()
+            .UseKestrel();
     }
 }
